8b725c855939347a9dd0cf8803de9a1d2e917eff
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-11 10:04:17 -0400

    added more apps

--- Server/cantina/settings.py
+++ Server/cantina/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'consumables.apps.ConsumablesConfig',
+    'servers.apps.ServersConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Server/cantina/urls.py
+++ Server/cantina/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('consumables/', include('consumables.urls')),
+    path('servers/', include('servers.urls')),
     path('admin/', admin.site.urls),
 ]
