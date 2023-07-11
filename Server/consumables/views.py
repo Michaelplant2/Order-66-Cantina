eca673148168ba88b55717b61878474627e994d3
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-11 16:43:54 -0400

    added nav bar

--- Server/consumables/views.py
+++ Server/consumables/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
 
+
+from .models import Consumable
+
+
 def index(request):
+    consumables = Consumable.objects.order_by('name').filter(is_published=True)
     dict = {
-        "value1": 1,
-        "value2": 2,
-        "value3": 3,
-        "value4": 4,
-        "value5": 5
+        Consumable
     }
 
     return JsonResponse(request, dict)
