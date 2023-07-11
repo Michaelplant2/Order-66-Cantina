ca392b955f35982ef941ada30b6feb4ffbb17f44
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-11 11:40:24 -0400

    edited admin page

--- Server/consumables/admin.py
+++ Server/consumables/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Consumable
 
-# Register your models here.
+class ConsumableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_published', 'price', 'cuisine_type')
+    list_display_links = ('id', 'name')
+    list_filter = ('cuisine_type',)
+    list_editable = ('is_published',)
+    search_fields = ('name', 'description', 'price')
+    list_per_page = 25
+
+admin.site.register(Consumable, ConsumableAdmin)
--- Server/servers/admin.py
+++ Server/servers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Servers
 
-# Register your models here.
+class ServersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_mvp', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Servers, ServersAdmin)
