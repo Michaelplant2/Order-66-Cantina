2218851888652284a36ea5b09de838a07d20ac0a
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-13 12:32:36 -0400

    added html

--- consumables/urls.py
+++ consumables/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='consumables'),
     path('<int:consumable_id>', views.consumable, name='consumable'),
-    path('search', views.search, name='search'),
+    # path('search', views.search, name='search'),
 ]
