f24651525250ba1638796afc1cb6fc61d9da52d3
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-18 15:30:20 -0400

    finished index

--- consumables/urls.py
+++ consumables/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='consumables'),
     path('<int:consumable_id>', views.consumable, name='consumable'),
-    # path('search', views.search, name='search'),
 ]
--- consumables/views.py
+++ consumables/views.py
@@ -6,14 +6,14 @@ from .models import Consumable
 def index(request):
     consumables = Consumable.objects.order_by('name').filter(is_published=True)
 
-    paginator = Paginator(consumables, 3)
+    paginator = Paginator(consumables, 6)
     page = request.GET.get('page')
     paged_consumables = paginator.get_page(page)
 
     context = {
         'consumables': paged_consumables
     }
-
+    
     return render(request, 'consumables/consumables.html', context)
 
 def consumable(request, consumable_id):
