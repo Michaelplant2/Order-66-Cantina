83585e38bb3c7db9027eb1f360f0b34fe24f1150
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-19 12:33:07 -0400

    finished main pages, worked logins

--- consumables/views.py
+++ consumables/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Consumable
 
 
 def index(request):
     consumables = Consumable.objects.order_by('name').filter(is_published=True)
 
-    paginator = Paginator(consumables, 6)
-    page = request.GET.get('page')
-    paged_consumables = paginator.get_page(page)
-
     context = {
-        'consumables': paged_consumables
+        'consumables': consumables
     }
     
     return render(request, 'consumables/consumables.html', context)
--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from consumables.models import Consumable
 from servers.models import Server
 
 def index(request):
-    consumables = Consumable.objects.all().filter(is_published=True)[:3]
+    consumables = Consumable.objects.order_by('-id').filter(is_published=True)[:5]
     servers = Server.objects.order_by('-hire_date')
     mvp_servers = Server.objects.all().filter(is_mvp=True)
 
