86abbbfc6960d6b3686fe4febe025f11a190f5d1
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-07-27 16:44:09 -0400

    nav responsiveness

--- cantina/static/js/scripts.js
+++ cantina/static/js/scripts.js
@@ -1,10 +1,16 @@
 function myFunction() {
   let x = document.getElementById("myTopnav");
+  let y = document.getElementById("navBar");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
-  }
+  };
+  if (y.className === "navbar") {
+    y.className += " center";
+  } else {
+    y.className = "navbar";
+  };
 }
 
 // Message Display
